2969d06d616b83415871918f6744d6c67932e74d
Oliwier-M <276085@student.pwr.edu.pl>
2024-03-08 09:31:59 +0100

    Added manage records fragment layout

--- app/src/main/java/com/example/vaccinationapp/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/example/vaccinationapp/ui/history/HistoryViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.vaccinationapp.ui.home
+package com.example.vaccinationapp.ui.history
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class HistoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is history Fragment"
     }
     val text: LiveData<String> = _text
 }
---
+++ app/src/main/java/com/example/vaccinationapp/ui/managerecords/ManageRecordsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.vaccinationapp.ui.managerecords
+
+import androidx.lifecycle.ViewModel
+
+class ManageRecordsViewModel : ViewModel() {
+
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is add new Fragment"
+//    }
+//    val text: LiveData<String> = _text
+
+}
